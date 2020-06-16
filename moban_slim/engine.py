a6545eab8ceb0de23c7177c7ce1f1d7491afab09
chfw <wangc_2011@hotmail.com>
2020-06-16 22:06:39 +0100

    :green_heart: pass lint check

--- moban_slim/engine.py
+++ moban_slim/engine.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
-from jinja2 import Environment, FileSystemLoader, Template
-from moban.externals import file_system
-from slimish_jinja import SlimishExtension
-
+from jinja2 import Template, Environment
 from jinja2_fsloader import FSLoader
+from moban.externals import file_system
+
+from slimish_jinja import SlimishExtension
 
 
 class EngineSlim(object):
